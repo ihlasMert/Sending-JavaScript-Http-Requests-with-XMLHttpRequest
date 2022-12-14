209d1ab37e834cb2a5e68e642c3262ace777768f
İHLASMERT <ihlas1mert@gmail.com>
2022-12-14 21:07:22 +0300

    maked dinamyc get mothod

--- xhr.js
+++ xhr.js
@@ -1,7 +1,19 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
+const sendHttpRequest = (method, url)=>{
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json"
+    xhr.onload=()=>{
+        const data = xhr.response
+        console.log(xhr.response)
+    }
+    xhr.send()
+}
 
-const getData = () => {};
+const getData = () => {
+    sendHttpRequest("GET", "https://reqres.in/api/users")
+};
 
 const sendData = () => {};
 
